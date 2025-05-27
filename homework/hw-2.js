01fa62da8d05e1068d029e9c1b854cc601617f70
ante-luu <aalusem@gmail.com>
2025-05-27 11:52:14 +0300

    feat: add homework-3 and clean up old files

--- homework/hw-2.js
+++ homework/hw-2.js
@@ -1,3 +1,67 @@
+// Задание 1
+let a1 = 10;
+alert(a1);
+a1 = 20;
+alert(a1);
+
 // Задание 2
 let firstIPhoneYear = 2007;
-alert(firstIPhoneYear); 
+alert(firstIPhoneYear);
+
+// Задание 3
+let jsCreator = "Brendan Eich";
+alert(jsCreator);
+
+// Задание 4
+let a2 = 10;
+let b2 = 2;
+alert(a2 + b2); // сумма
+alert(a2 - b2); // разность
+alert(a2 * b2); // произведение
+alert(a2 / b2); // частное
+
+// Задание 5
+let result = Math.pow(2, 5);
+alert(result);
+
+// Задание 6
+let a3 = 9;
+let b3 = 2;
+alert(a3 % b3);
+
+// Задание 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+// Задание 9
+let user = {
+    name: "John",
+    age: 25,
+    isAdmin: true
+};
+
+// Задание 10
+let name = prompt("Как вас зовут?");
+alert(`Привет, ${name}!`);
+
+// Дополнительное задание
+let number = prompt("Загадайте любое число");
+let doubled = number * 2;
+alert(doubled);
+let plusTen = doubled + 10;
+alert(plusTen);
+let divided = plusTen / 2;
+alert(divided);
+let final = divided - number;
+alert(final);
+alert("Ответ равен 5"); 
--- homework/hw-9.js
+++
@@ -1,6 +0,0 @@
-// Задание 9
-let user = {
-    name: "John",
-    age: 25,
-    isAdmin: true
-}; 
