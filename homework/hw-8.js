03a05c6b9fd0a1c6bc3498d9d1334317b3c0a129
ante-luu <aalusem@gmail.com>
2025-05-29 15:45:03 +0300

    Add homework 8 with JavaScript tasks (commented out)

---
+++ homework/hw-8.js
@@ -0,0 +1,87 @@
+// Задание 1
+/*
+const people = [
+   { name: 'Глеб', age: 29 },
+   { name: 'Анна', age: 17 },
+   { name: 'Олег', age: 7 },
+   { name: 'Оксана', age: 47 }
+];
+
+// Сортировка по возрасту
+console.log(people.sort((a, b) => a.age - b.age));
+*/
+
+// Задание 2
+/*
+function isPositive(number) {
+    return number > 0;
+}
+
+function isMale(person) {
+    return person.gender === 'male';
+}
+
+function filter(arr, ruleFunction) {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (ruleFunction(arr[i])) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
+
+// Тестирование функции filter
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people2 = [
+   {name: 'Глеб', gender: 'male'},
+   {name: 'Анна', gender: 'female'},
+   {name: 'Олег', gender: 'male'},
+   {name: 'Оксана', gender: 'female'}
+];
+
+console.log(filter(people2, isMale));
+*/
+
+// Задание 3
+/*
+function printDateFor30Seconds() {
+    const interval = setInterval(() => {
+        console.log(new Date());
+    }, 3000);
+
+    setTimeout(() => {
+        clearInterval(interval);
+        console.log('30 секунд прошло');
+    }, 30000);
+}
+*/
+
+// Задание 4
+/*
+function delayForSecond(callback) {
+    setTimeout(callback, 1000);
+}
+
+delayForSecond(function () {
+   console.log('Привет, Глеб!');
+});
+*/
+
+// Задание 5
+/*
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if(cb) {  cb(); }
+    }, 1000)
+}
+
+function sayHi (name) {
+    console.log(`Привет, ${name}!`);
+}
+
+// Исправленный код
+delayForSecond(() => sayHi('Глеб'));
+*/ 
